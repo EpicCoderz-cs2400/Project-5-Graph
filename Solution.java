b0ab9564931efdcec4d463bfc8769a8742e60938
Michael Smith <98797413+Madzswb21@users.noreply.github.com>
2022-05-09 21:48:24 -0700

    Update Solution.java
    setting up the graph

--- Solution.java
+++ Solution.java
@@ -5,3 +5,18 @@ import java.util.Arrays;
 
 public class Solution {
 
+        private static char key;
+
+        public static void main(String[] args) {
+
+                //              adj list representation
+                                LinkedHashMap<Character,ArrayList<Character>> adjList = new LinkedHashMap<>();
+                                adjList.put('A',new ArrayList<>(List.of('D','B')));
+                                adjList.put('B',new ArrayList<>(List.of('E')));
+                                adjList.put('C',new ArrayList<>(List.of('B')));
+                                adjList.put('D',new ArrayList<>(List.of('G')));
+                                adjList.put('E',new ArrayList<>(List.of('H','F')));
+                                adjList.put('F',new ArrayList<>(List.of('C', 'H')));
+                                adjList.put('G' ,new ArrayList<>(List.of('H')));
+                                adjList.put('H' ,new ArrayList<>(List.of('I')));
+                                adjList.put('I' ,new ArrayList<>(List.of('F')));
