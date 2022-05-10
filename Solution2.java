1ac8480deac1214d6ea6bbc65cb74b542432dec1
Michael Smith <98797413+Madzswb21@users.noreply.github.com>
2022-05-09 22:27:17 -0700

    Update Solution2.java

--- Solution2.java
+++ Solution2.java
@@ -17,3 +17,28 @@ public class Solution2 {
                 }
         }
 
+        public static void main(String[] args) {
+
+//              adj list representation
+                LinkedHashMap<Character,ArrayList<Character>> adjList = new LinkedHashMap<>();
+                adjList.put('A',new ArrayList<>(List.of('D','B')));
+                adjList.put('B',new ArrayList<>(List.of('E')));
+                adjList.put('C',new ArrayList<>(List.of('B')));
+                adjList.put('D',new ArrayList<>(List.of('G')));
+                adjList.put('E',new ArrayList<>(List.of('H','F')));
+                adjList.put('F',new ArrayList<>(List.of('C', 'H')));
+                adjList.put('G' ,new ArrayList<>(List.of('H')));
+                adjList.put('H' ,new ArrayList<>(List.of('I')));
+                adjList.put('I' ,new ArrayList<>(List.of('F')));
+                LinkedHashSet<Character> visited = new LinkedHashSet<>();
+                System.out.println("Dfs order: ");
+//              calls the dfs method to prints the dfs order
+                dfs('A',visited,adjList);
+                System.out.println();
+                
+                
+
+        
+        }
+
+}
