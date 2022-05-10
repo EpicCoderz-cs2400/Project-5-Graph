303d7624be555fa500e53be6d4fdbfe14823c861
Michael Smith <98797413+Madzswb21@users.noreply.github.com>
2022-05-09 22:22:14 -0700

    Update Solution2.java

--- Solution2.java
+++ Solution2.java
@@ -3,3 +3,17 @@ import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.ArrayList;
 
+
+public class Solution2 {
+//      displays the dfs order of the graph
+        public static void dfs(char curr,LinkedHashSet<Character> visited,LinkedHashMap<Character,ArrayList<Character>> adjList) {
+                System.out.println(curr);
+                visited.add(curr);
+                for(char ver: adjList.get(curr)) {
+                        if(!visited.contains(ver)) {
+                                dfs(ver,visited,adjList);
+                                
+                        }
+                }
+        }
+
